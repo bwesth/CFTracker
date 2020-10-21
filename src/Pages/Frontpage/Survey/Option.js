d86c148edb5b095a7abe89eaa93abf861e119326
Bjørn Westh <bjwe@itu.dk>
2020-10-21 08:59:49 +0200

    Options componant comment

--- src/Pages/Frontpage/Survey/Option.js
+++ src/Pages/Frontpage/Survey/Option.js
@@ -4,6 +4,11 @@ import { UserContext } from "../../../Main/UserContext";
 export default (props) => {
   const [data, setData] = useContext(UserContext).surveyChoices;
 
+  /* 
+  Syncing to context by utilizing context useState hook function, 
+  and checking the context each time. All <Option /> loads every time a change is many in any one componant.
+  TODO: Look into individual <Option /> state (context) managment for less remounting.
+  */
   return (
     <>
       <p>{props.text}</p>
@@ -12,7 +17,7 @@ export default (props) => {
         type="radio"
         formID={props.formID}
         onChange={() => {
-          setData({...data, [props.formID] : props.text});
+          setData({ ...data, [props.formID]: props.text });
         }}
         checked={data[props.formID] === props.text}
       />
