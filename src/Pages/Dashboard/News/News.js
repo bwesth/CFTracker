14c054e0e3b383379ed190bd9ac7afe41162633a
Bjørn Westh <bjwe@itu.dk>
2020-11-03 12:29:50 +0100

    -

--- src/Pages/Dashboard/News/News.js
+++ src/Pages/Dashboard/News/News.js
@@ -1,5 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
+import Article from "./Article";
+import apidata from "./apidata";
 
-export default () => <div>
-    <h1>News dashboard menu</h1>
-</div>
+export default () => {
+  // var unirest = require("unirest");
+
+  // var req = unirest(
+  //   "GET",
+  //   "https://rapidapi.p.rapidapi.com/api/search/NewsSearchAPI"
+  // );
+
+  // req.query({
+  //   pageSize: "10",
+  //   q: "climate change",
+  //   autoCorrect: "true",
+  //   pageNumber: "1",
+  //   toPublishedDate: "null",
+  //   fromPublishedDate: "null",
+  // });
+
+  // req.headers({
+  //   "x-rapidapi-key": "16ab   df544amsha0481382609fe81p13c2a6jsn6126ebe31040",
+  //   "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
+  //   useQueryString: true,
+  // });
+
+  const [newsArray, setNewsArray] = useState(apidata.value),
+    [newsBody, setNewsbody] = useState("");
+
+  // req.end(function (res) {
+  //   if (res.error) throw new Error(res.error);
+  //     setNewsArray(res.body.value)
+  //   console.log(res.body);
+  // });
+
+  function showArticle(index) {
+    setNewsbody(newsArray[index].body);
+  }
+
+  const test = newsArray[0];
+  console.log(test);
+
+  const style = {
+    display: "grid",
+    // flexDirection: "row",
+  }
+
+  return ( <>
+    <div style={style}>
+      {newsArray.map((item, index) => (
+        <Article data={item} click={showArticle} index={index} />
+      ))}
+      </div>
+      <p>{newsBody}</p>
+    </>
+  );
+};
