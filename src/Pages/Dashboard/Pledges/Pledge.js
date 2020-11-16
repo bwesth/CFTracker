cb0a4c011bc32b1251b6d98b8b768ff248207014
Bjørn Westh <bjwe@itu.dk>
2020-11-16 15:43:12 +0100

    Pledge description dropdown take 1

--- src/Pages/Dashboard/Pledges/Pledge.js
+++ src/Pages/Dashboard/Pledges/Pledge.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default ({ symbol, head, body, tonnes, color }) => {
-  const Tonnes = () => 
-    <div className="pledgeBox" style={{backgroundColor: color}}>
+export default ({ symbol, head, body, tonnes, color, img, description }) => {
+  const [descriptionVisible, setDescriptionVisible] = useState(false);
+
+  const Tonnes = () => (
+    <div className="pledgeBox" style={{ backgroundColor: color }}>
       <h1>{tonnes}</h1>
-      <p>tonnes of C02 saved</p>
-    </div>;
-  ;
-  /* ToDo:  
- <h1 className="pledgeBox"  style={{backgroundColor: color}}>{symbol}</h1>
-    */
-  return (
-    <div className="pledge">
-      <h1 className="pledgeBox"  style={{backgroundColor: color}}>{symbol}</h1>
-      <div>
-      <h2>{head}</h2>
-      <p>{body}</p>
-      </div>
-      {Tonnes()}
+      <p>
+        tonnes of
+        <br /> C02 saved
+      </p>
     </div>
+  );
+
+  return (
+    <>
+      <div
+        className="pledge"
+        onClick={() => setDescriptionVisible((visibility) => !visibility)}
+      >
+        <h1 className="pledgeBox" style={{ backgroundColor: color }}>
+          {symbol}
+        </h1>
+        <div>
+          <h2>{head}</h2>
+          <p>{body}</p>
+        </div>
+        {Tonnes()}
+      </div>
+      {descriptionVisible && (
+        <div className="pledge-description">
+          <img src={img} />
+          <p>{description}</p>
+        </div>
+      )}
+    </>
   );
 };
--- src/Pages/Dashboard/Pledges/Pledges.js
+++ src/Pages/Dashboard/Pledges/Pledges.js
@@ -1,24 +1,56 @@
 import React from "react";
 import Wrapper from "../../../Shared/Wrapper/Wrapper";
-import cycle from './cycle.bmp'
-import Pledge from './Pledge'
+import Pledge from "./Pledge";
+import placeholder from '../../../placeholderimage.png'
 
 export default () => {
-    let data = [
-        {symbol: "🚲", head: "Cyclical Commute", body: "I will ride my bicycle to work every day.", tonnes: 1.1, color: "#02C39A"},
-        {symbol: "✈️", head: "Infrequent Flier", body: "I will abstain from long distance flights for a year.", tonnes: 0.5, color: "#F46A67"},
-        {symbol: "🥬", head: "Going Vegan", body: "I will stick to a vegan diet 5 days of the week.", tonnes: 2.3, color: "#53AED5"}
+  let data = [
+    {
+      symbol: "🚲",
+      head: "Cyclical Commute",
+      body: "I will ride my bicycle to work every day.",
+      tonnes: 1.1,
+      color: "#02C39A",
+      img: placeholder,
+      description: `Cycling is a great healthy habit! Start cycling to work instead of driving, and you’ll save your own body as well as the planet! Keep it up for a month and you’ll save a whole tonne of C02!`,
+    },
+    {
+      symbol: "✈️",
+      head: "Infrequent Flier",
+      body: "I will abstain from long distance flights for a year.",
+      tonnes: 0.5,
+      color: "#F46A67",
+      img: placeholder,
+      description: `Cycling is a great healthy habit! Start cycling to work instead of driving, and you’ll save your own body as well as the planet! Keep it up for a month and you’ll save a whole tonne of C02!`,
 
-    ]
-    let pledges = data.map((pledge => <Pledge symbol={pledge.symbol} head={pledge.head} body={pledge.body} tonnes={pledge.tonnes} color={pledge.color} />))
+    },
+    {
+      symbol: "🥬",
+      head: "Going Vegan",
+      body: "I will stick to a vegan diet 5 days of the week.",
+      tonnes: 2.3,
+      color: "#53AED5",
+      img: placeholder,
+      description: `Cycling is a great healthy habit! Start cycling to work instead of driving, and you’ll save your own body as well as the planet! Keep it up for a month and you’ll save a whole tonne of C02!`,
+    },
+  ];
+  let pledges = data.map((pledge) => (
+    <Pledge
+      symbol={pledge.symbol}
+      head={pledge.head}
+      body={pledge.body}
+      tonnes={pledge.tonnes}
+      color={pledge.color}
+      img={pledge.img}
+      description={pledge.description}
+    />
+  ));
   /* ToDo:  
 
     */
   return (
     <Wrapper direction="column">
-      <div className="pledges">
-      {pledges}
-      </div>
+      <div className="pledges">{pledges}</div>
     </Wrapper>
   );
 };
