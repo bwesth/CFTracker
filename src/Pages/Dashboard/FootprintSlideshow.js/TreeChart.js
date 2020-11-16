fc4401d18f05e60abe103e8500736dee65375b97
John Henrik Muller <jhmu@itu.dk>
2020-11-16 18:00:26 +0100

    Added basic Tree graph, probably needs to be completely revised later.

--- src/Pages/Dashboard/FootprintSlideshow.js/TreeChart.js
+++ src/Pages/Dashboard/FootprintSlideshow.js/TreeChart.js
@@ -1,4 +1,4 @@
-import graph from '../../../placeholderimage.png'
+import { VictoryScatter } from 'victory';
 import React from 'react'
 
 export default () => {
@@ -9,7 +9,32 @@ export default () => {
 
     
     return <div className="slide">
-        <img style={{height: "200px", width: "auto"}} src={graph} />
-        <p>Trees</p>
+        <div className="treeGraph">
+        <VictoryScatter
+            animate={{
+                duration: 2000,
+                onLoad: { duration: 1000 }
+            }}
+            style={{ data: { fill: "#c43a31" } }}
+            bubbleProperty="amount"
+            maxBubbleSize={25}
+            minBubbleSize={5}
+            data={[
+                { x: 1, y: 2, amount: 30 },
+                { x: 2, y: 3, amount: 40 },
+                { x: 3, y: 5, amount: 25 },
+                { x: 4, y: 4, amount: 10 },
+                { x: 5, y: 7, amount: 45 }
+            ]}
+        />
+        </div>
+
+        <div className="chartInfo">
+            <h2>Trees</h2>
+            <p>Y'all save this many trees</p>
+            <p>Y'all save this many trees</p>
+            <p>Y'all save this many trees</p>
+        </div>
+
     </div>
 }
