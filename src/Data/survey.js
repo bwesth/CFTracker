1ecd56788cd210905d7c3b515a5682c2d8138a52
John Henrik Muller <jhmu@itu.dk>
2020-11-18 09:32:31 +0100

    Slight cleanup, removed a few unneeded comments.

--- src/Data/survey.js
+++ src/Data/survey.js
@@ -30,8 +30,6 @@ export default {
       ],
     },
 
-    //To make the calculator accurate we would have to compare this offset value vs. their previous choice somehow.
-    //Currently it just adds less if you offset more.
     {
       name: "Transport: Flights",
       question: "What percentage of your flights do you offset?",
@@ -158,41 +156,3 @@ export default {
   ],
 };
 //END OF LINE//------------------------------------------------------------------------------------
-
-
-//Below are the questions an answers used in the WWF climate survey, which is an excellent model to follow for the question format.
-
-//WWF QUESTIONS AND ANSWERS
-
-//FOOD
-// How would you best describe your diet? Meat every meal, meat some meals, meat very rarely, no beef, vegetarian, vegan
-// How much food do you waste? 0, 10, 20, 30, 30+
-// How often do you buy locally produced food? A lot/Some/Don't care
-// How much do you spend on take out 0 pounds, 1-10, 10-50, 50+
-
-//TRANSPORT
-//What kind of vehicle do you travel in most often as a driver or passenger? Car, Motorbike, Public Transport, Cycle, Walk
-//How many hours per week do you spend on trains for personal use including commuting? None, 2hrs, 2-5, 5-15, 15-25, 25+
-//Same as above but bus, dont, 1hr, 1-3, 3-6, 6-10, 10+
-//In the last year how many return flights have you made to the following regions? Domestic, up to 1250, up to 2500, up to 5500, up to 9000, up to 17500
-//What percentage of your flights do you offset? None, 25, 50, 75, 100
-
-//HOUSING
-//What kind of house do you live in? detached, semi detached, terrace, flat
-//How many bedrooms does your house have? 1, 2, 3, 4 or more
-//How many people live in your house (17 and over)? 1, 2, 3, 4, 5 or more
-//How do you usually heat your home? Gas, Oil, Electricity, Wood
-//Is your electricity on a green tariff? I don't know, no, yes but less than 100%, yes 100%
-//Do you regularly turn off your lights and appliances? Yes, no
-//How warn do you keep your home in the winter? below 14, 14-17, 18-21, over 21
-//Any energey efficient improvements? energy saving lightbulds, loft insulation, etc
-
-//GOODS
-//In the past 12 months, have you bought any of these new household items? TV, laptop or PC/Large item of furniture/washin machine..blah blah
-//In a typical month how much do you spend on clothes and footwear? 0, 1-50, 50-150, 150+
-//In a month, how much on pets and pet food? none, 0-10, 10-30, 30+
-//How much do you spend on health, beauty and grooming products? 0-10/10-50/50+
-//How much do you spend on phone, internet and TV? 0, 1-130, 30-60, 60+
-//Which types of waste do you recycle or compost? Food/Paper/Plastic/Tin Cans/Glass
-
-//LOADS of questions
