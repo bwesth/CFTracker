4f9c94e4626b8be367ec751c3e80231f7aaeb21f
NikolajMollerup <NikolajMollerup@gmail.com>
2020-11-18 11:52:42 +0100

    Fixed style on sign up, and login form

--- src/Pages/Frontpage/LoginForm.js
+++ src/Pages/Frontpage/LoginForm.js
@@ -21,30 +21,43 @@ export default (props) => {
   });
 
   return (
-    <div className="popup-wrapper">
       <form
         className="login-form"
         onSubmit={(e) => e.preventDefault() && false}
       >
-        <input
-          id="email"
-          name="email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          ref={register}
-        />
-        <input
-          id="password"
-          name="password"
-          type="text"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          ref={register}
-        />
-        <input type="submit" onClick={login} />
+        <div className="logInH">
+          <h1>Log In</h1>
+        </div>
+        <div className="Inputs">
+          <div className="Email">
+            <p>Email</p>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              ref={register}
+            />
+          </div>
+          <div className="Password">
+            <p>Password</p>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              ref={register}
+            />
+          </div >
+          <div className="text">
+          <p>Forgot username or password?</p>
+          <p>Don’t have an account? Sign up!</p>
+          </div>
+          <input className="logInButton" type="submit" onClick={login} />
+        </div>
       </form>
-    </div>
   );
 
   async function login() {
