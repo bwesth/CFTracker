39c11e1ff991b77fb3896dc21c9c50b1997e2bd2
NikolajMollerup <NikolajMollerup@gmail.com>
2020-12-19 18:58:14 +0100

    Fixed "add Pleged button"

--- src/Pages/Frontpage/Frontpage.js
+++ src/Pages/Frontpage/Frontpage.js
@@ -26,7 +26,7 @@ export default ({ scrollToTop }) => {
             There is now no doubt that climate change will have a profound
             effect on our future.
           </p>
-          <p>Help us to save the planet, one person at a time</p>
+          <p>Help us to save the planet, one person at a time.</p>
         </div>
         <div className="middelBox">
           <h1>Become The Solution</h1>
